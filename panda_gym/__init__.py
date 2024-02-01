f9846d4d58eb2d28ea7e2ae5e0500174e133f67f
RiicK3d <40563427+RiicK3d@users.noreply.github.com>
2024-02-01 16:49:55 +0100

    added Reach and Peginsertion Options Registration

--- panda_gym/__init__.py
+++ panda_gym/__init__.py
@@ -10,6 +10,7 @@ ENV_IDS = []
 for task in ["Reach", "Slide", "Push", "PickAndPlace", "Stack", "Flip", "PegInsertion"]:
     for reward_type in ["sparse", "dense"]:
         for control_type in ["ee", "joints"]:
+
             reward_suffix = "Dense" if reward_type == "dense" else ""
             control_suffix = "Joints" if control_type == "joints" else ""
             env_id = f"Panda{task}{control_suffix}{reward_suffix}-v3"
@@ -20,5 +21,19 @@ for task in ["Reach", "Slide", "Push", "PickAndPlace", "Stack", "Flip", "PegInse
                 kwargs={"reward_type": reward_type, "control_type": control_type},
                 max_episode_steps=100,
             )
+
+            # adding randomize starting point to Reach and PegInsertion task
+            if task in ["Reach", "PegInsertion"]:
+                random_suffix = 'Random'
+                reward_suffix = "Dense" if reward_type == "dense" else ""
+                control_suffix = "Joints" if control_type == "joints" else ""
+                env_id = f"Panda{task}{control_suffix}{reward_suffix}{random_suffix}-v3"
+
+                register(
+                    id=env_id,
+                    entry_point=f"panda_gym.envs:Panda{task}Env",
+                    kwargs={"reward_type": reward_type, "control_type": control_type, "random_init_pos": True},
+                    max_episode_steps=100,
+                )
 
             ENV_IDS.append(env_id)
