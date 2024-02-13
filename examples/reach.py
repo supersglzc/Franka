b088da4913301f98086ea9ebab3618ece558b024
Steven <zl2993@columbia.edu>
2024-02-13 17:21:41 +0100

    move obstacle up

--- examples/reach.py
+++ examples/reach.py
@@ -3,15 +3,16 @@ import numpy as np
 import panda_gym
 import time 
 
-env = gym.make("PandaReach-v3", render_mode="human")
+env = gym.make("PandaReachJointsDenseRandom-v3", render_mode="human")
 
 observation, info = env.reset()
 
 for _ in range(1000):
     action = env.action_space.sample()
-    action = np.zeros_like(action)
+    # action = np.zeros_like(action)
     observation, reward, terminated, truncated, info = env.step(action)
-    time.sleep(1/120)
+    print(reward)
+    time.sleep(0.1)
     if terminated or truncated:
         print("reset")
         print(truncated)
